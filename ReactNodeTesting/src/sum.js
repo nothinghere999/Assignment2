36d1d037a28e8e68ad3e24fb3bbbfc72263a9f9a
Ubuntu <ubuntu@ip-172-31-18-237.eu-north-1.compute.internal>
2023-12-24 22:01:21 +0000

    workflow created

--- ReactNodeTesting/src/sum.js
+++
@@ -1,9 +0,0 @@
-
-export default function sum() {
-  var s = 0;
-  for (var i=0; i < arguments.length; i++) {
-      s += arguments[i];
-  }
-  return s;
-}
-  
--- ReactNodeTesting/src/todoTitle.test.js
+++
@@ -1,7 +0,0 @@
-import todoTitle from "./todoTitle";
-
-test('should return the first name from the api', async () => {
-    const result = await todoTitle(1);
-    expect(result.title).toBe('delectus aut autem');
-});
-
